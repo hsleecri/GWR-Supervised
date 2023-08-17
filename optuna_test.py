a8e26525c5210f26efda32b1389cbcb0c9560c80
hyeonsu <criticaller@hanyang.ac.kr>
2023-08-17 21:31:37 +0900

    modified

--- optuna_test.py
+++ optuna_test.py
@@ -27,9 +27,9 @@ def objective(trial, ds_iris,export_network_directory):
     beta = trial.suggest_float('beta', 0.01, 0.95)
     epsilon_b = trial.suggest_float('epsilon_b', 0.1, 0.3)
     epsilon_n = trial.suggest_float('epsilon_n', 0.0005, 0.0015)
-    # hab_threshold = trial.suggest_float('hab_threshold', 0.05, 0.2)
-    # tau_b = trial.suggest_float('tau_b', 0.1, 0.5)
-    # tau_n = trial.suggest_float('tau_n', 0.05, 0.2)
+    hab_threshold = trial.suggest_float('hab_threshold', 0.05, 0.2)
+    tau_b = trial.suggest_float('tau_b', 0.1, 0.5)
+    tau_n = trial.suggest_float('tau_n', 0.05, 0.2)
     max_age = trial.suggest_int('max_age', 100, 10000)
     num_context = trial.suggest_int('num_context', 1, 15)
     #'''
@@ -37,8 +37,8 @@ def objective(trial, ds_iris,export_network_directory):
     # Create and train network with suggested hyperparameters
     my_net = GammaGWR()
     my_net.init_network(ds=ds_iris, random=False, num_context=num_context)
-    # my_net.train_ggwr(ds=ds_iris, epochs=epochs, a_threshold=a_threshold, beta=beta, l_rates=[epsilon_b, epsilon_n], hab_threshold=hab_threshold, tau_b=tau_b, tau_n=tau_n, max_age=max_age) #모든 하이퍼파라미터를 교정하고 싶을 때
-    my_net.train_ggwr(ds=ds_iris, epochs=epochs, a_threshold=a_threshold, beta=beta, l_rates=[epsilon_b, epsilon_n], hab_threshold=1, tau_b=0.3, tau_n=0.1, max_age=max_age) #성능에 직접적으로 큰 영향을 미치는 하이퍼파라미터를 교정할 때
+    my_net.train_ggwr(ds=ds_iris, epochs=epochs, a_threshold=a_threshold, beta=beta, l_rates=[epsilon_b, epsilon_n], hab_threshold=hab_threshold, tau_b=tau_b, tau_n=tau_n, max_age=max_age) #모든 하이퍼파라미터를 교정하고 싶을 때
+    # my_net.train_ggwr(ds=ds_iris, epochs=epochs, a_threshold=a_threshold, beta=beta, l_rates=[epsilon_b, epsilon_n], hab_threshold=1, tau_b=0.3, tau_n=0.1, max_age=max_age) #성능에 직접적으로 큰 영향을 미치는 하이퍼파라미터를 교정할 때
     """
     기존 연구 코드에서 explicit하게 hyperparameter라고 써놓은 것들: num_context=1,epochs=15,a_threshold=0.85, beta=0.7, l_rates=[0.2, 0.001]
     기존 연구 코드에서 코드 내부에 특정값으로 설정되어 있던 것들: hab_threshold = 0.1, tau_b = 0.3, tau_n = 0.1, max_neighbors = 6, max_age = 600
@@ -94,7 +94,7 @@ def main(data_file, output_directory, export_network_directory, n_trials):
     pio.write_image(plot_slice, output_directory + 'slice_plot.png')
 
 if __name__ == "__main__":
-    data_file = 'C:\\Users\\hslee\\Desktop\\dataset\\HYEONSU\\4공정\\CSV\\4공정_FRONT_CYCLE.mp4pose_world_interpolated_visibility제거_하반신제거_레이블_첫행제거_레이블값1뺐음.csv'
+    data_file = 'C:\\Users\\hslee\\Desktop\\dataset\\HYEONSU\\4공정\\CSV\\4공정_FRONT_CYCLE.mp4pose_world_interpolated_visibility제거_하반신제거_sorted_features_by_std_deviation_상위10개_레이블_첫행제거.csv'
     output_directory = 'C:\\Users\\hslee\\Desktop\\dataset\\HYEONSU\\4공정\\PNG\\'
     export_network_directory = 'C:\\Users\\hslee\\Desktop\\dataset\\HYEONSU\\4공정\\Pickle\\' 
     main(data_file, output_directory, export_network_directory, n_trials=150) #set the number of trials in study
